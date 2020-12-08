7ccba2ddd9ecd3687321f4e92803d96e358ea4df
Oleg Budaev <t0r1n88@mail.ru>
2020-12-08 13:01:25 +0800

    Рабочая версия 0.8

--- dodger.py
+++ dodger.py
@@ -3,12 +3,11 @@ import csv
 from docxcompose.composer import Composer
 from docx import Document as Document_compose
 import os
-import time
 
 
-def combine_all_docx(filename_master,files_list):
+def combine_all_docx(filename_master, files_list):
     # Функция для объединения документов
-    number_of_sections=len(files_list)
+    number_of_sections = len(files_list)
     master = Document_compose(filename_master)
     composer = Composer(master)
     for i in range(0, number_of_sections):
@@ -17,16 +16,18 @@ def combine_all_docx(filename_master,files_list):
     composer.save("Сертификаты.docx")
 
 
-
 # Считываем csv файл, не забывая что екселввский csv разделен на самомо деле не запятыми а точкой с запятой
-reader = csv.DictReader(open('resources/1.csv'), delimiter=';')
+reader = csv.DictReader(open('resources/data.csv'), delimiter=';')
 # Конвертируем объект reader в список словарей
 data = list(reader)
 
 # Создаем в цикле документы
 for row in data:
     doc = DocxTemplate('resources/template.docx')
-    context = {'lastname': row['lastname'], 'firstname': row['firstname'], 'number': row['number']}
+    context = {'lastname': row['lastname'], 'firstname': row['firstname'], 'number': row['number'],
+               'profession': row['profession'], 'date_expiry': row['date_expiry'],'date_issue':row['date_issue'],
+               'qualification': row['qualification'],
+               'category': row['category'], 'name_prep': row['name_prep'], 'name_dir': row['name_dir']}
     doc.render(context)
     doc.save(f'{row["lastname"]} {row["firstname"]}.docx')
 
@@ -44,5 +45,5 @@ composer = Composer(master)
 for i in range(1, number_of_sections):
     doc_temp = Document_compose(files[i])
     composer.append(doc_temp)
-composer.save("Сертификаты.docx")
+composer.save("Все Свидетельства в одном файле.docx")
 # Объединяем документы
