a0df93607f4bb273a6f64d3cee2f72c329a8bbd0
Oleg Budaev <t0r1n88@mail.ru>
2022-11-14 16:42:00 +0800

    Начал работу над автоматической обработкой писем

--- get_attach_email.py
+++ get_attach_email.py
@@ -3,21 +3,66 @@
 
 """
 from imap_tools import MailBox, AND
+from xls2xlsx import XLS2XLSX
+import os
+
+
+path_to_end = 'C:/Данные/'
 
 # Get date, subject and body len of all emails from INBOX folder
+not_used = ['Спам','Отправленные','Черновики','Корзина']
+
+
 with MailBox('imap.mail.ru').login('myschool@copp03.ru', 'irjkf@_22') as mailbox:
-# with MailBox('imap.mail.ru').login('budaev_oleg@copp03.ru', 'jsBcBmB9Jb1c6NHgR2eE') as mailbox:
-    for msg in mailbox.fetch():
-        for att in msg.attachments:
-            if att.filename.endswith('.xlsx') or att.filename.endswith('.xls'):
-                print(att.filename, att.content_type)
-                with open('C:/3/{}'.format(att.filename), 'wb') as f:
-                    f.write(att.payload)
+    for f in mailbox.folder.list():
+        if f.name not in not_used:
+            print(f.name)
+            mailbox.folder.set(f.name)
+            for msg in mailbox.fetch():
+                print(f' Subject {msg.subject}') # заголовок письма
+                print(f' From {msg.from_}') # адрес почты отправителя
+                print(f' Date {msg.date}') # время отправки
+
+                print('***************')
+                for att in msg.attachments:
+                    if att.filename.endswith('.xlsx'):  # 2007 excel
+                        # print(att.filename, att.content_type)
+
+                        with open(f'{path_to_end}{att.filename}','wb') as f:
+                            f.write(att.payload)
+                    elif att.filename.endswith(('.xls')):  # 2003 excel
+                        file_name = att.filename.replace('.xls', '')
+                        with open(f'{path_to_end}{att.filename}', 'wb') as f:
+                            f.write(att.payload)
+                        out = XLS2XLSX(f'{path_to_end}{att.filename}') # конвертируем в
+                        out.to_xlsx((f'{path_to_end}{file_name}.xlsx')) # сохраняем
+                        os.remove(f'{path_to_end}{att.filename}') # удаляем файл xls чтобы не мешался
+                    else:
+                        continue
+#
+#
+# # with MailBox('imap.mail.ru').login('budaev_oleg@copp03.ru', 'jsBcBmB9Jb1c6NHgR2eE') as mailbox:
+#     for msg in mailbox.fetch():
+#         print(f' Subject {msg.subject}') # заголовок письма
+#         print(f' From {msg.from_}') # адрес почты отправителя
+#         print(f' Date {msg.date}') # время отправки
+#
+#         print('***************')
+#         for att in msg.attachments:
+#             if att.filename.endswith('.xlsx'):  # 2007 excel
+#                 # print(att.filename, att.content_type)
+#
+#                 with open(f'{path_to_end}{att.filename}','wb') as f:
+#                     f.write(att.payload)
+#             elif att.filename.endswith(('.xls')):  # 2003 excel
+#                 file_name = att.filename.replace('.xls', '')
+#                 with open(f'{path_to_end}{att.filename}', 'wb') as f:
+#                     f.write(att.payload)
+#                 out = XLS2XLSX(f'{path_to_end}{att.filename}') # конвертируем в
+#                 out.to_xlsx((f'{path_to_end}{file_name}.xlsx')) # сохраняем
+#                 os.remove(f'{path_to_end}{att.filename}') # удаляем файл xls чтобы не мешался
+#             else:
+#                 continue
 
     # for msg in mailbox.fetch():
 
-        # for att in msg.attachments:
-        #     if att.filename.endswith('.xlsx'):
-        #         print(att.filename, att.content_type)
-        #         with open('C:/3/{}'.format(att.filename), 'wb') as f:
-        #             f.write(att.payload)
