3860314451a8503dd56d42438bde501553d20e59
Oleg Budaev <t0r1n88@mail.ru>
2022-11-16 10:12:59 +0800

    •	Переработать получение расширения файла через split[-1] + •	Названия колонок.+ •	При ошибках работа бы продолжалась + •	Названия листов.+

--- get_attach_email.py
+++ get_attach_email.py
@@ -30,30 +30,36 @@ not_used = ['Спам','Отправленные','Черновики','Корз
 cols_df = list(range(23))
 df = pd.DataFrame(columns=cols_df) # базовый датафрейм
 us_df = pd.DataFrame(columns=['Откуда прислан файл','Название файла','Время отправки','Тип ошибки']) # Датафрейм для неправильных файлов
-try:
-    with tempfile.TemporaryDirectory() as temp_dir:
+
+with tempfile.TemporaryDirectory() as temp_dir:
 
 
-        with MailBox('imap.mail.ru').login('myschool@copp03.ru', 'irjkf@_22') as mailbox:
-            for f in mailbox.folder.list():
-                if f.name not in not_used:
-                    mailbox.folder.set(f.name)
-                    for msg in mailbox.fetch():
-                        # print(f' Subject {msg.subject}') # заголовок письма
-                        # print(f' From {msg.from_}') # адрес почты отправителя
-                        # print(f' Date {msg.date}') # время отправки
-                        #
-                        # print('***************')
-                        msg_from = msg.from_ # получаем адрес почты отправителя
-                        msg_date = msg.date
-                        for att in msg.attachments:
-                            if att.filename.endswith('.xlsx') or att.filename.endswith('.xls'):  # проверяем на расширение
+    with MailBox('imap.mail.ru').login('myschool@copp03.ru', 'irjkf@_22') as mailbox:
+        for f in mailbox.folder.list():
+            if f.name not in not_used:
+                mailbox.folder.set(f.name)
+                for msg in mailbox.fetch():
+                    # print(f' Subject {msg.subject}') # заголовок письма
+                    # print(f' From {msg.from_}') # адрес почты отправителя
+                    # print(f' Date {msg.date}') # время отправки
+                    #
+                    # print('***************')
+                    msg_from = msg.from_ # получаем адрес почты отправителя
+                    msg_date = msg.date
+                    for att in msg.attachments:
+                        try: # оборачиваем в try except чтобы при ошибках процесс продолжался
+                            extension = att.filename.split('.')[-1].lower() # получаем расширение файла и делаем его строчным(для случаев наподобие XLSX)
+                            print(msg_from)
+                            print(att.filename)
+                            print(extension)
+                            print('*********')
+                            if extension == 'xlsx' or extension == 'xls':  # проверяем на расширение
 
-                                if att.filename.endswith('.xlsx'): # Сохраняем во временную папку
+                                if extension == 'xlsx': # Сохраняем во временную папку
                                     work_file_name = att.filename
                                     with open(f'{temp_dir}{att.filename}','wb') as f:
                                         f.write(att.payload)
-                                elif att.filename.endswith('.xls'): # конвертируем и сохраняем
+                                elif extension == 'xls': # конвертируем и сохраняем
                                     work_file_name = att.filename.replace('.xls', '.xlsx')
                                     with open(f'{temp_dir}{att.filename}', 'wb') as f:
                                         f.write(att.payload)
@@ -69,10 +75,11 @@ try:
                                 check_file = getMergedCellVal(wb[first_list], wb[first_list]['L2']) # получаем значение ячейки,если совпадает то файл нужный нам
                                 if check_file == standard_str:
                                     if len(wb.sheetnames) == 1: # Проверяем длину
+                                        name_sheet = wb.sheetnames[0] # получаем название листа
                                         name_org = wb[first_list]['B5'].value # получаем значение ячейки B5
-                                        print(name_org)
                                         temp_df = pd.read_excel(f'{temp_dir}{work_file_name}',skiprows=4,header=None,dtype=str) # считываем датафрейм
                                         temp_df[0] = msg_from
+                                        temp_df.insert(1,'Тип таблицы',name_sheet)
                                     else:
                                         len_sheets = len(wb.sheetnames)
                                         temp_df = pd.DataFrame(columns=list(range(23)))
@@ -81,8 +88,11 @@ try:
                                             try:
                                                 check_cols = ml_temp_df.iloc[:,1].any() # если есть хоть одно значение в колоноке 1 то добавляем эти данные
                                                 if check_cols:
+                                                    name_sheet = sheet
                                                     ml_temp_df[0] = msg_from
+                                                    ml_temp_df.insert(1, 'Тип таблицы', name_sheet)
                                                     temp_df=pd.concat([temp_df,ml_temp_df],ignore_index=True)
+
                                             except IndexError:
                                                 continue
 
@@ -104,16 +114,35 @@ try:
                                 temp_bad['Время отправки'] = temp_bad['Время отправки'].apply(lambda x: pd.to_datetime(x).date())
 
                                 us_df = pd.concat([us_df,temp_bad],ignore_index=True)
-except OSError:
-    temp_bad = pd.DataFrame(columns=['Откуда прислан файл','Название файла','Время отправки','Тип ошибки'],
-                            data=(
-                            msg_from, att.filename, msg_date, 'Ошибка при обработке файла !!!'))  # создаем датафрейм с данными ошибки
-    temp_bad['Время отправки'] = temp_bad['Время отправки'].apply(lambda x: pd.to_datetime(x).date())
-    us_df = pd.concat([us_df,temp_bad], ignore_index=True)
+                        except:
+
+                            temp_bad = pd.DataFrame(columns=['Откуда прислан файл','Название файла','Время отправки','Тип ошибки'],
+                                                    data=(
+                                                    msg_from, att.filename, msg_date, 'Ошибка при обработке файла !!!'))  # создаем датафрейм с данными ошибки
+                            temp_bad['Время отправки'] = temp_bad['Время отправки'].apply(lambda x: pd.to_datetime(x).date())
+                            us_df = pd.concat([us_df,temp_bad], ignore_index=True)
+                            continue
+# except OSError:
+#     temp_bad = pd.DataFrame(columns=['Откуда прислан файл','Название файла','Время отправки','Тип ошибки'],
+#                             data=(
+#                             msg_from, att.filename, msg_date, 'Ошибка при обработке файла !!!'))  # создаем датафрейм с данными ошибки
+#     temp_bad['Время отправки'] = temp_bad['Время отправки'].apply(lambda x: pd.to_datetime(x).date())
+#     us_df = pd.concat([us_df,temp_bad], ignore_index=True)
 
 t = time.localtime()
 current_time = time.strftime('%H_%M_%S', t)
 
 df.rename(columns={0:'Откуда прислан файл',1:'Название учреждения'},inplace=True)
+df.insert(1,'Лист',df['Тип таблицы'])
+df.drop(columns=['Тип таблицы'],inplace=True)
+
+df.rename(columns={2:'Тип',3:'Наименование',4:'Краткое наименование населенного пункта',5:'Наименование муниципального района, муниципального/городского округа',
+                   6:'Регион',7:'ИНН',8:'ОГРН',9:'Email',10:'Телефон',11:'Согласие директора',12:'ФИО директора',13:'Должность директора',
+                   14:'Телефон директора',15:'СНИЛС директора',16:'Email директора',17:'Согласие администратора',18:'ФИО администратора',
+                   19:'Должность администратора',20:'Телефон администратора',21:'СНИЛС администратора',22:'Email администратора'},inplace=True)
+# df.columns = ['Откуда прислан файл','Лист','Полное наименование','Тип','Краткое наименование населенного пункта','Район,округ','Регион','ИНН','ОГРН',
+#               'Email','Телефон','Согласие директора',
+#               'ФИО директора','Должность','Телефон директора','СНИЛС директора','Email директора',
+#               'Согласие администратора','ФИО администратора','Должность администратора','Телефон администратора','СНИЛС администратора','Email администратора']
 df.to_excel(f'{path_to_end}Данные организаций для ФГИС Моя Школа от {current_time}.xlsx',index=False)
 us_df.to_excel(f'{path_to_end}Ошибки и некорректные файлы для ФГИС Моя Школа от {current_time}.xlsx',index=False)
